7b2f390695f1d2896be7fa780aba9498e05712ab
Nirmal <nirmalnavitas@gmail.com>
2018-04-24 15:26:37 +0530

    formatted module.ts
    Co-Authored-By: dhanukanadeeshan <dhanukanadeeshan@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
 // import { ListPage } from '../pages/list/list';
-import { VehiclePage } from '../pages/vehicle/vehicle';
-import { AccomodationPage } from '../pages/accomodation/accomodation';
-import { Guide } from '../pages/guide/guide';
-import { RouteFinder } from '../pages/route-finder/route-finder';
-import { Places } from '../pages/places/places';
-import { Equipment } from '../pages/equipment/equipment';
+import {VehiclePage} from '../pages/vehicle/vehicle';
+import {AccomodationPage} from '../pages/accomodation/accomodation';
+import {Guide} from '../pages/guide/guide';
+import {RouteFinder} from '../pages/route-finder/route-finder';
+import {Places} from '../pages/places/places';
+import {Equipment} from '../pages/equipment/equipment';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { Hotels} from '../pages/hotels/hotels';
-import { CallNumber } from '@ionic-native/call-number';
-
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {Hotels} from '../pages/hotels/hotels';
+import {CallNumber} from '@ionic-native/call-number';
 
 
 @NgModule({
@@ -56,4 +55,5 @@ import { CallNumber } from '@ionic-native/call-number';
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
