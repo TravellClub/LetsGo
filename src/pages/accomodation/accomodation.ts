cb12d8f25e54f5cf966a5a703450a5047a4a729b
Nirmal <nirmalnavitas@gmail.com>
2018-04-24 15:06:32 +0530

    accomodation list
    Co-Authored-By: thayalini <thayalini@users.noreply.github.com>

--- src/pages/accomodation/accomodation.ts
+++ src/pages/accomodation/accomodation.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import{ Hotels } from'../hotels/hotels';
-import { CallNumber } from '@ionic-native/call-number';
 
 
 @Component({
@@ -10,16 +9,38 @@ import { CallNumber } from '@ionic-native/call-number';
 })
 export class AccomodationPage {
 
-  constructor(public navCtrl: NavController, public callNumber: CallNumber) {
+  items = [];
 
+  constructor(public navCtrl: NavController) {
   }
   openhotel(){
     this.navCtrl.push(Hotels);
-  } 
+  }
   opencall(){
     console.log("calling number");
-    this.callNumber.callNumber("0775817987", true)
-    .then(res => console.log('Launched dialer!', res))
-  .catch(err => console.log('Error launching dialer', err));
+  //   this.callNumber.callNumber("0775817987", true)
+  //   .then(res => console.log('Launched dialer!', res))
+  // .catch(err => console.log('Error launching dialer', err));
   }
+
+  loadData(){
+    console.log("Loading accommodation data");
+      this.items = [{
+        name : "Hotel Name",
+        address : "Address of the hotel",
+        contact : "0711225544",
+        rating : 5.0
+      },{
+        name : "Hotel 2",
+        address : "Address of the hotel 2",
+        contact : "0777445555",
+        rating : 4.0
+      }
+      ]
+  }
+
+  ionViewDidLoad(){
+    this.loadData();
+  }
+
 }
