b5e7a417c6c2aacbc660526fbd9cb4f963b47369
Nirmal <nirmalnavitas@gmail.com>
2018-04-24 15:32:33 +0530

    formatted fix
    Co-Authored-By: dhanukanadeeshan <dhanukanadeeshan@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,81 +5,46 @@ import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 import {MyApp} from './app.component';
 import {HomePage} from '../pages/home/home';
 // import { ListPage } from '../pages/list/list';
-<<<<<<< HEAD
-import { VehiclePage } from '../pages/vehicle/vehicle';
-import { AccomodationPage } from '../pages/accomodation/accomodation';
-import { Guide } from '../pages/guide/guide';
-import { RouteFinder } from '../pages/route-finder/route-finder';
-import { Places } from '../pages/places/places';
-import { Login } from '../pages/login/login';
-import { Equipment } from '../pages/equipment/equipment';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { Directions } from '../pages/directions/directions';
-import { Signup } from '../pages/signup/signup';
-
-=======
 import {VehiclePage} from '../pages/vehicle/vehicle';
 import {AccomodationPage} from '../pages/accomodation/accomodation';
 import {Guide} from '../pages/guide/guide';
 import {RouteFinder} from '../pages/route-finder/route-finder';
 import {Places} from '../pages/places/places';
+import {Login} from '../pages/login/login';
 import {Equipment} from '../pages/equipment/equipment';
->>>>>>> 7b2f390695f1d2896be7fa780aba9498e05712ab
 
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
-import {Hotels} from '../pages/hotels/hotels';
-import {CallNumber} from '@ionic-native/call-number';
+import {Directions} from '../pages/directions/directions';
+import {Signup} from '../pages/signup/signup';
+import {Hotels} from "../pages/hotels/hotels";
 
+let pageArr = [MyApp,
+  HomePage,
+  // ListPage,
+  VehiclePage,
+  AccomodationPage,
+  Guide,
+  RouteFinder,
+  Places,
+  Equipment,
+  Login,
+  Directions,
+  Signup,
+  Hotels];
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    // ListPage,
-    VehiclePage,
-    AccomodationPage,
-    Guide,
-    RouteFinder,
-    Places,
-    Equipment,
-<<<<<<< HEAD
-    Login,
-    Directions,
-    Signup
-=======
-    Hotels
->>>>>>> 7b2f390695f1d2896be7fa780aba9498e05712ab
-  ],
+  declarations: pageArr,
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    // ListPage,
-    VehiclePage,
-    AccomodationPage,
-    Guide,
-    RouteFinder,
-    Places,
-    Equipment,
-<<<<<<< HEAD
-    Login,
-    Directions,
-    Signup
-=======
-    Hotels
->>>>>>> 7b2f390695f1d2896be7fa780aba9498e05712ab
-  ],
+  entryComponents: pageArr,
   providers: [
     StatusBar,
     SplashScreen,
-    CallNumber,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
