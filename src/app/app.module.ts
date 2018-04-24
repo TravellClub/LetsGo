27693271d7fdd4dde1a600b0bb88c76013dc5a9b
DhanukaNadeeshan <dhanuka.dn@gmail.com>
2018-04-24 15:29:53 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 import {MyApp} from './app.component';
 import {HomePage} from '../pages/home/home';
 // import { ListPage } from '../pages/list/list';
+<<<<<<< HEAD
 import { VehiclePage } from '../pages/vehicle/vehicle';
 import { AccomodationPage } from '../pages/accomodation/accomodation';
 import { Guide } from '../pages/guide/guide';
