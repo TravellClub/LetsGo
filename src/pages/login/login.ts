ee351c056f994c0bd9ee6602bdb4b0f26e77cc25
DhanukaNadeeshan <dhanuka.dn@gmail.com>
2018-05-11 09:38:11 +0530

    cerate authentications & alerts in login and signup pages

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { RouteFinder } from '../route-finder/route-finder';
 import { Signup } from '../signup/signup';
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 // $IMPORTSTATEMENT
 
 /**
@@ -17,17 +19,49 @@ import { Signup } from '../signup/signup';
 })
 export class Login {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  itemList: Observable<any>;
+  user = {};
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    public afDatabase: AngularFireDatabase,public alerCtrl: AlertController) {
+
+    this.itemList = afDatabase.list('/user').valueChanges();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Login');
   }
 
-  onLoginClicked(){
-    this.navCtrl.push(RouteFinder);
+  onLoginClicked(user) {
+    console.log("User login : ", user);
+    this.itemList.forEach(element => {
+      element.forEach(u => {
+        console.log("user : ", u);
+        if (u.email == user.username) {
+          if (user.password == u.password) {
+            this.navCtrl.push(RouteFinder);
+          }else{
+            let alert = this.alerCtrl.create({
+              title: 'Ops!',
+              message: 'Password that you enterd is incorrect!',
+              buttons: ['Ok']
+            });
+            alert.present()
+          
+          }
+        }else{
+          let alert = this.alerCtrl.create({
+            title: 'Ops!',
+            message: 'Username that you enterd is incorrect!',
+            buttons: ['Ok']
+          });
+          alert.present()
+        }
+      })
+    })
   }
-  openSignup(){
+
+
+  openSignup() {
     this.navCtrl.push(Signup);
   }
 
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, Input } from '@angular/core';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
 
 
 
@@ -11,26 +12,58 @@ import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms'
   templateUrl: 'signup.html',
 })
 export class Signup {
-   public signupForm;
+  public signupForm;
 
-   langs;
-   langform;
+  public user = {};
+  langs;
+  langform;
+  itemList: AngularFireList<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, _form:FormBuilder) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, _form: FormBuilder
+    , public afDatabase: AngularFireDatabase,public alerCtrl: AlertController) {
 
-    // this.signupForm = this.signupForm.group({
-    //   "email":["",Validators.required],
-    //   "password":["",Validators.required]
-    // })
+    this.itemList = afDatabase.list('/user');
+    // this.items = this.itemList.valueChanges();
 
     this.langform = new FormGroup({
-      "langs": new FormControl({value: 'business', disabled:false})
+      "langs": new FormControl({ value: 'business', disabled: false })
     })
+
+ 
 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Signup');
+  }
+
+  onSubmit(user) {
+    console.log('ionViewDidLoad Signup');
+    console.log('User : ', this.user);
+    let userRef = this.itemList.push({});
+
+    userRef.set({
+      id: userRef.key,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      password: user.password,
+      contact_number: user.contact_number,
+      address: user.address,
+
+    });
+    let alert = this.alerCtrl.create({
+      title: 'Congratulations!',
+      message: 'You have signed up successfully',
+      buttons: ['Ok']
+    });
+    alert.present()
+
+    // this.signupForm.reset();
+
+
+
+
   }
 
 
